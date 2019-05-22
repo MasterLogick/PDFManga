c008a99c2ecfd82ba4b7909afe4778eff78e5811
MasterLogick <vovasst_m@mail.ru>
2019-05-22 18:58:28 +0300

    fast downloading

--- src/Main.java
+++ src/Main.java
@@ -14,98 +14,109 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    static String name_url = /*"11221-.hackg.u..html";*/ "11874-accel-world.html";
+    static int amount = Integer.MAX_VALUE;
+    static int threadCount = 10;
+
     public static void main(String[] args) {
         PDDocument doc = new PDDocument();
-        String[] urls = new String[]{
-                "/online/473888-kenja-no-mago_v13_ch57.html",
-                "/online/473887-kenja-no-mago_v13_ch56.html",
-                "/online/465148-kenja-no-mago_v13_ch55.html",
-                "/online/465147-kenja-no-mago_v13_ch54.html",
-                "/online/465146-kenja-no-mago_v13_ch53.html",
-                "/online/465145-kenja-no-mago_v13_ch52.html",
-                "/online/441765-kenja-no-mago_v13_ch51.html",
-                "/online/439597-kenja-no-mago_v13_ch50.html",
-                "/online/434938-kenja-no-mago_v13_ch49.html",
-                "/online/434527-kenja-no-mago_v13_ch48.html",
-                "/online/426993-kenja-no-mago_v13_ch47.5.html",
-                "/online/417794-kenja-no-mago_v13_ch47.html",
-                "/online/417793-kenja-no-mago_v13_ch46.html",
-                "/online/415276-kenja-no-mago_v13_ch45.html",
-                "/online/414026-kenja-no-mago_v13_ch44.html",
-                "/online/411961-kenja-no-mago_v13_ch43.html",
-                "/online/411960-kenja-no-mago_v13_ch42.html",
-                "/online/403472-kenja-no-mago_v13_ch41.html",
-                "/online/400703-kenja-no-mago_v13_ch40.html",
-                "/online/400481-kenja-no-mago_v13_ch39.html",
-                "/online/390034-kenja-no-mago_v13_ch38.html",
-                "/online/389804-kenja-no-mago_v13_ch37.html",
-                "/online/389651-kenja-no-mago_v13_ch36.html",
-                "/online/380788-kenja-no-mago_v13_ch35.html",
-                "/online/380787-kenja-no-mago_v13_ch34.html",
-                "/online/380786-kenja-no-mago_v13_ch33.html",
-                "/online/380785-kenja-no-mago_v13_ch32.html",
-                "/online/380784-kenja-no-mago_v13_ch31.html",
-                "/online/337022-kenja-no-mago_v13_ch30.html",
-                "/online/337021-kenja-no-mago_v13_ch29.html",
-                "/online/337020-kenja-no-mago_v13_ch28.html",
-                "/online/349578-kenja-no-mago_v13_ch27.html",
-                "/online/339257-kenja-no-mago_v13_ch26.html",
-                "/online/316951-kenja-no-mago_v13_ch25.html",
-                "/online/316950-kenja-no-mago_v12_ch24.html",
-                "/online/316949-kenja-no-mago_v12_ch23.html",
-                "/online/313905-magis-grandson_v11_ch22.html",
-                "/online/313904-magis-grandson_v11_ch21.html",
-                "/online/310149-magis-grandson_v10_ch20.html",
-                "/online/310148-magis-grandson_v10_ch19.html",
-                "/online/305376-kenja-no-mago_v9_ch18.6.html",
-                "/online/305375-kenja-no-mago_v9_ch18.5.html",
-                "/online/305374-kenja-no-mago_v9_ch18.html",
-                "/online/305373-kenja-no-mago_v9_ch17.html",
-                "/online/305372-kenja-no-mago_v8_ch16.html",
-                "/online/304094-kenja-no-mago_v8_ch15.html",
-                "/online/301909-kenja-no-mago_v7_ch14.html",
-                "/online/301908-kenja-no-mago_v7_ch13.html",
-                "/online/299982-kenja-no-mago_v6_ch12.html",
-                "/online/299197-kenja-no-mago_v6_ch11.html",
-                "/online/296332-kenja-no-mago_v5_ch10.html",
-                "/online/296331-kenja-no-mago_v5_ch9.html",
-                "/online/296330-kenja-no-mago_v4_ch8.html",
-                "/online/296329-kenja-no-mago_v4_ch7.html",
-                "/online/290762-kenja-no-mago_v3_ch6.html",
-                "/online/289586-kenja-no-mago_v3_ch5.html",
-                "/online/288144-kenja-no-mago_v2_ch4.html",
-                "/online/284119-kenja-no-mago_v2_ch3.html",
-                "/online/281706-magis-grandson_v1_ch2.html",
-                "/online/271734-magis-grandson_v1_ch1.html",
-                "/online/270128-kenja-no-mago_v1_ch0.html"
-                };
-String[] arr = new String[urls.length];
-        for (int i = 0; i < urls.length; i++) {
-            arr[i]=urls[urls.length-1-i];
+        String[] select = body();
+        ArrayList<String> urls = urls(select);
+        System.out.println(urls.size() + " chapters");
+        ArrayList<String> imgURLs = imgURLs(urls);
+        System.out.println(imgURLs.size() + " images");
+        PDPage[] buffer = new PDPage[imgURLs.size()];
+        int size = imgURLs.size() / threadCount;
+        AtomicInteger count = new AtomicInteger();
+        for (int i = 0; i < threadCount - 1; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                int a = finalI;
+                for (int j = 0; j < size; j++) {
+                    try {
+                        BufferedImage bi = ImageIO.read(new URL(imgURLs.get(size * a + j)));
+                        PDPage page = new PDPage();
+                        page.setCropBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                        page.setMediaBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                        PDImageXObject pdImage = LosslessFactory.createFromImage(doc, bi);
+                        PDPageContentStream contents = new PDPageContentStream(doc, page);
+                        contents.drawImage(pdImage, 0, 0);
+                        contents.close();
+                        buffer[size * a + j] = page;
+                        System.out.println("Page " + (size * a + j) + " copied. " + (count.incrementAndGet()) + " of " + imgURLs.size());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
-        urls=arr;
-        for (String url :
-                urls) {
-            HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://mangachan.me"+url)).build();
-            HttpResponse<String> response = null;
-            try {
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            String sub = response.body().substring(response.body().indexOf("\"fullimg\":[\"")+"\"fullimg\":[\"".length());
-            sub=sub.substring(0,sub.indexOf("\",]"));
-            String[] uris = sub.split("\",\"");
-            for (String uri :
-                    uris) {
+        new Thread(() -> {
+            int a = threadCount - 1;
+            for (int j = 0; j < size + imgURLs.size() % threadCount; j++) {
                 try {
-                    BufferedImage bi = null;
-                    bi = ImageIO.read(new URL(uri));
+                    BufferedImage bi = ImageIO.read(new URL(imgURLs.get(size * a + j)));
+                    PDPage page = new PDPage();
+                    page.setCropBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                    page.setMediaBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                    PDImageXObject pdImage = LosslessFactory.createFromImage(doc, bi);
+                    PDPageContentStream contents = new PDPageContentStream(doc, page);
+                    contents.drawImage(pdImage, 0, 0);
+                    contents.close();
+                    buffer[size * a + j] = page;
+                    System.out.println("Page " + (size * a + j) + " copied. " + (count.incrementAndGet()) + " of " + imgURLs.size());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        while (count.get() != imgURLs.size()) {
+        }
+        for (PDPage bi :
+                buffer) {
+            if (bi == null) System.out.println("aaa");
+        }
+        System.out.println("completed");
+        int a = 0;
+        for (PDPage bi :
+                buffer) {
+            doc.addPage(bi);
+            System.out.println(++a + " page has been drew.");
+        }
+        try {
+            File f = new File("/home/user/" + name_url.substring(0, name_url.length() - ".html".length()) + ".pdf");
+            f.createNewFile();
+            f.delete();
+            f.createNewFile();
+            doc.save(f);
+            doc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*int bufferSize = 1024;
+        BufferedImage[] buffer = new BufferedImage[bufferSize];
+        int part = 0;
+        int i = 0;
+        int j = 0;
+        while (i < imgURLs.size()) {
+            for (j = 0; j < bufferSize && j < imgURLs.size(); j++, i++) {
+                try {
+                    buffer[j] = ImageIO.read(new URL(imgURLs.get(i)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(imgURLs.get(i) + " buffered. " + (((float) i) / imgURLs.size() * 100) + "%. " + i + " from " + imgURLs.size());
+            }
+            PDDocument doc = new PDDocument();
+            for (int k = 0; k < j; k++) {
+                BufferedImage bi = buffer[k];
+                try {
                     PDPage page = new PDPage();
                     page.setCropBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
                     page.setMediaBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
@@ -116,60 +127,112 @@ String[] arr = new String[urls.length];
                     contents = new PDPageContentStream(doc, page);
                     contents.drawImage(pdImage, 0, 0);
                     contents.close();
-                    System.out.println("Copied img: "+uri);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                buffer[k]=null;
+            }
+            PDFRenderer pdfRenderer = new PDFRenderer(doc);
+            PDDocument doc1 = new PDDocument();
+            try {
+                int pageCounter = 0;
+                for (PDPage page1 : doc.getPages()) {
+                    // note that the page number parameter is zero based
+                    BufferedImage bi = pdfRenderer.renderImageWithDPI(pageCounter, 144, ImageType.RGB);
+                    pageCounter++;
+                    PDPage page = new PDPage();
+                    page.setCropBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                    page.setMediaBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
+                    doc1.addPage(page);
+                    PDImageXObject pdImage = null;
+                    pdImage = LosslessFactory.createFromImage(doc1, bi);
+                    PDPageContentStream contents = null;
+                    contents = new PDPageContentStream(doc1, page);
+                    contents.drawImage(pdImage, 0, 0);
+                    contents.close();
+                    System.out.println("Page: " + pageCounter + " converted");
+                }
+
+                doc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                File f = new File("/home/user/" + name_url.substring(0, name_url.length() - ".html".length()) + "-part-" + part + ".pdf");
+                f.createNewFile();
+                f.delete();
+                f.createNewFile();
+                doc1.save(f);
+                doc1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.gc();
+        }*/
+    }
+
+    public static ArrayList<String> imgURLs(ArrayList<String> urls) {
+        ArrayList<String> ret = new ArrayList<>();
+        for (String url :
+                urls) {
+            HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://mangachan.me" + url)).build();
+            HttpResponse<String> response = null;
+            try {
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String sub = response.body().substring(response.body().indexOf("\"fullimg\":[\"") + "\"fullimg\":[\"".length());
+            sub = sub.substring(0, sub.indexOf("\",]"));
+            String[] uris = sub.split("\",\"");
+            for (String s :
+                    uris) {
+                ret.add(s);
+            }
+            System.out.println("Page: " + url + " parsed");
+        }
+        return ret;
+    }
+
+    public static String[] body() {
+        HttpClient cl = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+        HttpRequest rq = HttpRequest.newBuilder().uri(URI.create("https://mangachan.me/" + name_url)).build();
+        HttpResponse<String> rsp = null;
+        try {
+            rsp = cl.send(rq, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return rsp.body().split("\n");
+    }
+
+    public static ArrayList<String> urls(String[] select) {
+        ArrayList<String> r = new ArrayList<>();
+        for (String s :
+                select) {
+            if (s.contains("zaliv")) r.add(s);
+        }
+        amount = Math.max(0, Math.min(amount, r.size()));
+        String[] selectItems = new String[amount];
+        for (int i = 0; i < amount; i++) {
+            selectItems[i] = r.get(r.size() - 1 - i);
+        }
+        ArrayList<String> urls = new ArrayList<>();
+        for (String s :
+                selectItems) {
+            String splitLeft = "href=\'";
+            int a = s.indexOf(splitLeft);
+            if (a == -1) continue;
+            else {
+                s = s.substring(a + splitLeft.length());
+                urls.add(s.substring(0, s.indexOf("\'")));
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*try {
-            BufferedImage bi = null;
-            bi = ImageIO.read(new URL("http://t9.mangas.rocks/auto/21/53/83/013.png_res.jpg"));
-            PDPage page = new PDPage();
-            page.setCropBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
-            page.setMediaBox(new PDRectangle(bi.getWidth(), bi.getHeight()));
-            doc.addPage(page);
-            PDImageXObject pdImage = null;
-            pdImage = LosslessFactory.createFromImage(doc, bi);
-            PDPageContentStream contents = null;
-            contents = new PDPageContentStream(doc, page);
-            contents.drawImage(pdImage, 0, 0);
-            contents.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        */try {
-            File f = new File("/home/user/lollol.pdf");
-            f.createNewFile();
-            f.delete();
-            f.createNewFile();
-            doc.save(f);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return urls;
     }
 }
